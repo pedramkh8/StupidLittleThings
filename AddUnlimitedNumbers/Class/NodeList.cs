bea54a38eed8180f3f3abff136972861b39d782e
Pedram <pedram_kh8@outlook.com>
2020-10-28 02:14:03 +0330

    Add tow numbers with link list.

---
+++ AddUnlimitedNumbers/Class/NodeList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace AddUnlimitedNumbers.Class
+{
+	internal class NodeList
+	{
+		public int Value { get; set; }
+		public NodeList Next { get; set; }
+		public NodeList(int value = 0, NodeList next = null)
+		{
+			this.Next = next;
+			this.Value = value;
+		}
+
+		public NodeList(string value)
+		{
+			NodeList node = ToNodeList(value);
+			this.Next = node.Next;
+			this.Value = node.Value;
+		}
+
+		private NodeList ToNodeList(string value)
+		{
+			NodeList first = new NodeList();
+			NodeList current = first;
+			NodeList next = first;
+
+			foreach (var num in value)
+			{
+				current = next;
+				current.Value = int.Parse(num.ToString());
+				next = new NodeList();
+				current.Next = next;
+			}
+
+			current.Next = null;
+
+			return first;
+		}
+		 
+		internal NodeList Add(NodeList firstList, NodeList secondList)
+		{
+			int sum = 0;
+			int extra = 0;
+			NodeList first = new NodeList();
+			NodeList current = first;
+			NodeList next = first;
+
+			while (firstList != null || secondList != null)
+			{
+				current = next;
+				sum = (firstList?.Value ?? 0) + (secondList?.Value ?? 0) + extra;
+				current.Value = sum % 10;
+				extra = sum > 9 ? 1 : 0;
+				firstList = firstList?.Next;
+				secondList = secondList?.Next;
+				next = new NodeList();
+				current.Next = next;
+			}
+
+			current.Next = null;
+
+			if (extra == 1)
+			{
+				current.Next = new NodeList(1);
+			}
+
+			return first;
+		}
+	}
+}
---
+++ AddUnlimitedNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using AddUnlimitedNumbers.Class;
+using System;
+using System.Text.RegularExpressions;
+
+namespace AddUnlimitedNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Regex regex = new Regex(@"^\d+$");
+
+			while (true)
+			{
+			EnterFirstNumber:
+				Console.WriteLine("Enter The First Number:");
+				string firstNumber = Console.ReadLine();
+
+				if (!regex.IsMatch(firstNumber))
+				{
+					Console.WriteLine("Please Enter A Number!");
+					goto EnterFirstNumber;
+				}
+
+			EnterSecondNumber:
+				Console.WriteLine("Enter The Second Number:");
+				string secondNumber = Console.ReadLine();
+
+				if (!regex.IsMatch(secondNumber))
+				{
+					Console.WriteLine("Please Enter A Number!");
+					goto EnterSecondNumber;
+				}
+
+				NodeList first = new NodeList(firstNumber);
+				NodeList second = new NodeList(secondNumber);
+				NodeList result = first.Add(first, second);
+				string resultToShow = string.Empty;
+
+				while (result != null)
+				{
+					resultToShow += result.Value.ToString();
+					result = result.Next;
+				}
+
+				Console.WriteLine($"Result: {resultToShow}");
+			}
+		}
+	}
+}
