def9f958654f3cc520bc3b28ea67ba60ab6054a7
Pedram <pedram_kh8@outlook.com>
2020-10-28 17:31:34 +0330

    Add Find Median.

---
+++ MedianOfTwoSortedArray/MedianOfTwoSortedArray/Median.cs
@@ -0,0 +1,36 @@
+﻿namespace MedianOfTwoSortedArray
+{
+	public static class Median
+	{
+		/// <summary>
+		/// O(Log(n))
+		/// </summary>
+		/// <param name="nums1"></param>
+		/// <param name="nums2"></param>
+		/// <returns></returns>
+		public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+		{
+			int[] nums3 = new int[nums1.Length + nums2.Length];
+
+			for (int i = 0; i < nums1.Length; i++)
+			{
+				nums3[i] = nums1[i];
+			}
+
+			for (int i = 0; i < nums2.Length; i++)
+			{
+				nums3[nums1.Length + i] = nums2[i];
+			}
+
+			nums3 = nums3.MergeSort();
+
+			if ((nums3.Length % 2) == 1)
+			{
+				return nums3[nums3.Length / 2];
+			}
+
+			return (nums3[nums3.Length / 2 - 1] + nums3[nums3.Length / 2]) / 2d;
+		}
+
+	}
+}
---
+++ MedianOfTwoSortedArray/MedianOfTwoSortedArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MedianOfTwoSortedArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			while (true)
+			{
+				Console.WriteLine("Enter The First Array Cama Separate:");
+				string array = Console.ReadLine();
+				int[] nums1 = Regex
+								.Replace(array, @"\s+", "")
+								.Split(',').Select(x => Int32.Parse(x)).ToArray();
+
+				Console.WriteLine("Enter The Second Array Cama Separate:");
+				array = Console.ReadLine();
+				int[] nums2 = Regex
+								.Replace(array, @"\s+", "")
+								.Split(',').Select(x => Int32.Parse(x)).ToArray();
+
+
+				Console.WriteLine($"Result: {Median.FindMedianSortedArrays(nums1, nums2)}");
+			}
+		}
+	}
+}
